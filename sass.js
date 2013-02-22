510c449ed3f651028cfe29abb1ff664407a28a97
Eugene Mirotin <emirotin@gmail.com>
2013-02-22 23:20:16 +0300

    some logging

--- sass.js
+++ sass.js
@@ -4,11 +4,13 @@ try {
   if (fs.realpathSync(__dirname + '/build')) {
     // use the build version if it exists
     binding = require(__dirname + '/build/Release/binding');
+    console.log('Node-sass using Release binding');
   }
 } catch (e) {
   // default to a precompiled binary if no build exists
   var platform_full = process.platform+'-'+process.arch;
   binding = require(__dirname + '/precompiled/'+platform_full+'/binding');
+  console.log('Node-sass using precompiled binding');
 }
 if (binding === null) {
   throw new Error('Cannot find appropriate binary library for node-sass');
